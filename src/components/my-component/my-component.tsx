69063b84121d1b92762137cba47db28a034bb79c
sophielem <lematre.sophiegmail.com>
2019-05-17 16:44:05 +0200

    install d3 and test it

--- src/components/my-component/my-component.tsx
+++ src/components/my-component/my-component.tsx
@@ -1,9 +1,10 @@
 import { Component, Prop } from '@stencil/core';
+import * as d3 from "d3";
 
 @Component({
   tag: 'genomic-card',
   styleUrl: 'my-component.css',
-  shadow: true
+  shadow: false
 })
 export class MyComponent {
 // *************************** PROPERTY & CONSTRUCTOR ***************************
@@ -37,6 +38,18 @@ export class MyComponent {
     console.log((event.currentTarget as HTMLOptionElement).value);
   }
 
+  componentDidLoad() {
+    console.log("Loaded")
+    var data = [30, 86, 168, 281, 303, 365];
+
+    d3.select(".chart")
+      .selectAll("div")
+      .data(data)
+        .enter()
+        .append("div")
+        .style("width", function(d) {return d + "px"; })
+        .text(function(d) { return d; });
+  }
 
 // *************************** DISPLAY ***************************
   render() {
@@ -69,16 +82,16 @@ export class MyComponent {
         <p>
           {this.genomCard}
         </p>
-
         <svg width={this.width_svg} height={this.height_svg}>
         </svg>
 
       </div>,
 
+      <div class="chart"></div>,
+
       <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>,
       <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>,
-      <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>,
-      <script src="https://d3js.org/d3.v5.min.js"></script>
+      <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 ])
   }
 }
