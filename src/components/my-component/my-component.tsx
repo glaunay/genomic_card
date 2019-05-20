bd072e877b52cd12cdeca9f33aa1aa569804b64d
sophielem <lematre.sophiegmail.com>
2019-05-20 10:38:57 +0200

    Draw an arc

--- src/components/my-component/my-component.tsx
+++ src/components/my-component/my-component.tsx
@@ -40,15 +40,19 @@ export class MyComponent {
 
   componentDidLoad() {
     console.log("Loaded")
-    var data = [30, 86, 168, 281, 303, 365];
+    let arcGenerator = d3.arc();
 
-    d3.select(".chart")
-      .selectAll("div")
-      .data(data)
-        .enter()
-        .append("div")
-        .style("width", function(d) {return d + "px"; })
-        .text(function(d) { return d; });
+    let pathData = arcGenerator({
+      startAngle: 0,
+      endAngle: 6.5,
+      innerRadius: 190,
+      outerRadius: 200
+    })
+
+    d3.select("g")
+      .append('path')
+      .attr('d', pathData);
+
   }
 
 // *************************** DISPLAY ***************************
@@ -83,12 +87,9 @@ export class MyComponent {
           {this.genomCard}
         </p>
         <svg width={this.width_svg} height={this.height_svg}>
+          <g transform="translate(400, 210)"></g>
         </svg>
-
       </div>,
-
-      <div class="chart"></div>,
-
       <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>,
       <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>,
       <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
