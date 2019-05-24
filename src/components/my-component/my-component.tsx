4057c54f25c9ba7fd8be0e7fd80f78b59e596a5a
sophielem <lematre.sophiegmail.com>
2019-05-24 10:55:53 +0200

    import bootstrap & font-awesome via css

--- src/components/my-component/my-component.tsx
+++ src/components/my-component/my-component.tsx
@@ -332,10 +332,6 @@ export class MyComponent {
 
 
     return ([
-      // @ts-ignore
-      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous" />,
-      // @ts-ignore
-      <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous" />,
       <div style={{display: displayLoad}}>
         <strong> Loading ... </strong>
         <div class="spinner-grow text-info" role="status"></div>
