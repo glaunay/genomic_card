cc337fc7845ff3dbb94be4ab67876ca0b5fdd6a6
sophielem <lematre.sophiegmail.com>
2019-07-19 09:51:23 +0200

    add some comments

--- src/components/my-component/clusteringTree.ts
+++ src/components/my-component/clusteringTree.ts
@@ -1,4 +1,9 @@
-function nbWord(sequences: Object) {
+/**
+* Return the total number of sgRNA occurences given a dictionary
+* @param {Object} sequences dictionary of sgRNA with the list of coordinates
+* @returns {Number} the total number of sgRNA
+*/
+function nbWord(sequences: Object):number {
   // number of total sgRNA
   let sum: number=0;
   for (var i in sequences) {
@@ -8,21 +13,32 @@ function nbWord(sequences: Object) {
   return sum;
 }
 
-
-function updateNiv(nivMax: number, nivCurr: number, nbSec: number) {
+/**
+* Return the level of the section to create a beautyful sunburst
+* @param {Number} nivMax Maximal level of the sunburst
+* @param {Number} nivCurr Maximal level of the sunburst
+* @param {Number} nbSec Number of section by circle
+* @returns {Number} the correct level for the section to display a sunburst without null sections
+*/
+function updateNiv(nivMax: number, nivCurr: number, nbSec: number):number {
   let b = nivMax - nivCurr;
   return nivMax * Math.pow(nbSec, b);
 }
 
-
-function fname(children, nivMax, nbSec) {
+/**
+* Set the level of all node to create a sunburst
+* @param {Array} children Maximal level of the sunburst
+* @param {Number} nivMax Maximal level of the sunburst
+* @param {Number} nbSec Number of section by circle
+*/
+function setNiv(children:Array<Object>, nivMax:number, nbSec:number):void {
   for (var i in children){
     if (children[i].hasOwnProperty('niv')) {
       if (children[i]['niv'] < nivMax){
         children[i]['niv'] = updateNiv(nivMax, children[i]['niv'], nbSec)
       }
     } else {
-      fname(children[i]['children'], nivMax, nbSec)
+      setNiv(children[i]['children'], nivMax, nbSec)
     }
   }
 }
@@ -31,20 +47,31 @@ function fname(children, nivMax, nbSec) {
 export class TreeClustering {
   // the root of the tree
   root: Object;
-  // Genome's size, dictionary of sequences with their coordinates, the number of sector,
-  // the min number before represent it by reads
-  constructor(sizeGenome: number, sequences: any, nbSec: number, minWord: number) {
+  /**
+  * Set the level of all node to create a sunburst
+  * @param {number} sizeGenome Genome size
+  * @param {Object} sequences Dictionary of sequences with their coordinates
+  * @param {number} nbSec Number of section by circle
+  * @param {number} minWord Minimal word by section
+  */
+  constructor(sizeGenome: number, sequences: Object, nbSec: number, minWord: number) {
     const [children, nivMax] = this.constructTree(sizeGenome, sequences, nbSec, minWord, 1, 0);
     let totalWeight = 0;
     console.log('NIVEAU MAX : ' + nivMax);
     children.forEach(child => totalWeight += child.weight);
     this.root = {'min': 0, 'max': sizeGenome, 'children': children, 'weight': totalWeight};
-    fname(this.root['children'], nivMax, nbSec)
-
-
+    setNiv(this.root['children'], nivMax, nbSec)
   }
 
-  find(min, max, sequences) {
+  /**
+  * Find coordinates of sequences inside a section given
+  * @param {number} min Maximal level of the sunburst
+  * @param {number} max Maximal level of the sunburst
+  * @param {Object} sequences Dictionary of sequences with their coordinates
+  * @returns {Object} Dictionary of sequences with their coordinates which are inside the sector
+  defined by min and max arguments
+  */
+  find(min:number, max:number, sequences:Object):Object {
     // sequences in the sector
     let sequencesIn:Object = {};
     for (var seq in sequences) {
@@ -64,7 +91,17 @@ export class TreeClustering {
     return sequencesIn;
   }
 
-  constructTree(sizeSector: number, sequences: Object, nbSec: number, minWord: number, niv, minPos: number) {
+  /**
+  * Set the level of all node to create a sunburst
+  * @param {number} sizeSector Genome size
+  * @param {Object} sequences Dictionary of sequences with their coordinates
+  * @param {number} nbSec Number of section by circle
+  * @param {number} minWord Minimal word excepted
+  * @param {number} niv Current level
+  * @param {number} minPos The satrt coordinate of the section
+  * @returns {Array} A list of children of the node and the maximal level
+  */
+  constructTree(sizeSector: number, sequences: Object, nbSec: number, minWord: number, niv:number, minPos: number):Array<any> {
     // calcul interval
     let sizeSubSector: number = sizeSector / nbSec;
     let listChildren = [];
